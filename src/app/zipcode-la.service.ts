043d924d4681b37f2aa45caec22afff4982f52e5
Jorge Martínez Sánchez <jorge.estudios98@gmail.com>
2023-09-22 00:10:37 +0200

    clean

--- src/app/zipcode-la.service.ts
+++ src/app/zipcode-la.service.ts
@@ -9,21 +9,7 @@ import { Observable } from 'rxjs';
 export class ZipcodeLAService {
   constructor(private http: HttpClient) { }
   baseUrl = 'http://127.0.0.1:5000';
-  getTest(): string {
-    return "asdasdad";
-  }
-  getValue(number: number): string{
-    switch (number){
-      case 1 : return '#6ac728' ;
-      case 2 : return '#e9fc17' ;
-      case 3 : return '#fcb717' ;
-      case 4 : return '#fc6b17' ;
-      case 5 : return '#fc1717' ;
-      break;
-      default: return "#e81014";
-    }
-  }
-
+  
   getColorByZipCode(zipCode: string): Observable<{data: string}> {
     const url = `${this.baseUrl}/getColor?zipcode=${zipCode}`;
     const responseData = this.http.get<string>(url);
